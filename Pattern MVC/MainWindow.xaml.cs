fa415b96856c228d3db0827ecec98289a6eabfd0
nasty <nastytrifono03@gmail.com>
2023-01-15 19:20:08 +0300

    Добавьте файлы проекта.

---
+++ Pattern MVC/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pattern_MVC
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Model.tbCombo = Znac;
+            Model.rez = Rezultat;
+            Model.per = Pervoe;
+            Model.vtor = Vtoroe;
+            arifmetika.ItemsSource = Model.arifmetika;
+            arifmetika.SelectedIndex = 0;
+          
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Model.Rezultat = arifmetika.SelectedIndex;
+        }
+
+        private void arifmetika_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Model.Combo = arifmetika.SelectedIndex;
+        }
+    }
+}
---
+++ Pattern MVC/Model.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Pattern_MVC
+{
+    public static class Model
+    {
+        public static List<string> arifmetika = new List<string> { "Сложение", "Вычитание", "Умножение", "Деление" };
+        public static List<string> znaki = new List<string> { "+", "-", "*", "/" };
+        
+
+
+        public static TextBlock rez;
+        public static TextBlock tbCombo;
+        public static TextBox per;
+        public static TextBox vtor;
+        public static int Rezultat
+        {
+            set
+            {
+                try
+                {
+                    string prev = per.Text;
+                    double pervoe = Convert.ToDouble(prev);
+                    string vtoro = vtor.Text;
+                    double vtoroe = Convert.ToDouble(vtoro);
+                    double rezul;
+                    switch (value)
+                    {
+                        case 0:
+                            rezul = pervoe + vtoroe;
+                            rez.Text = rezul.ToString();
+                            break;
+                        case 1:
+                            rezul = pervoe - vtoroe;
+                            rez.Text = rezul.ToString();
+                            break;
+                        case 2:
+                            rezul = pervoe * vtoroe;
+                            rez.Text = rezul.ToString();
+                            break;
+                        case 3:
+                            rezul = pervoe / vtoroe;
+                            rez.Text = rezul.ToString();
+                            break;
+                    }
+                }
+                catch
+                {
+                    rez.Text = "Ошибка";
+                }
+            }
+        }
+        public static int Combo  // свойтсво для отображения фамилии в Combobox
+        {
+            set
+            {
+                tbCombo.Text = znaki[value];
+            }
+        }
+    }
+}
